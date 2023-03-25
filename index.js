8697beeb7b13658493ac8d2570aa48c3dbcc9f45
Jesewe <81848878+Jesewe@users.noreply.github.com>
2023-03-25 15:20:05 +0600

    Version : 1.1

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const SteamUser = require('steam-user');
-
 const user = new SteamUser();
 
 const logOnOptions = {
-	accountName: 'your-login',  //Enter here your account login
-	password: 'your-password'		 //Enter your password
+	accountName: 'login',  // Enter here your account login
+	password: 'password'		 // Enter here your password
 }
 
 user.logOn(logOnOptions);
-
 user.on('loggedOn', () => {
 	console.log(logOnOptions.accountName + ' - Successfully logged on');
-	user.setPersona(1);                 //1 - online, 7 - invisible
+	user.setPersona(7);                 //1 - online, 7 - invisible
 	user.gamesPlayed([730]);    //List app IDs
 });
+
+setInterval(() => {
+  console.log("+1 Hour");
+}, 60 * 60 * 1000);
